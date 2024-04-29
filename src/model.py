d885eae62e34d9d0330eca60b9dfe990dee3e85a
mandychen-astro <chenmandy2012@gmail.com>
2024-04-29 16:11:56 -0500

    Refactor SpectrumTransformerEncoder and SpectrumTransformerDecoder classes in model.py

--- src/model.py
+++ src/model.py
@@ -63,5 +63,58 @@ class ConvAutoencoder(nn.Module):
         return x
 
 
+class SpectrumTransformerEncoder(nn.Module):
+    def __init__(self, num_specpixles, embedding_dim):
+        super(SpectrumTransformerEncoder, self).__init__()
+        self.spec_embedding = nn.Linear(num_specpixles, embedding_dim)  
+        # self.positional_encodings = nn.Parameter(torch.zeros(1, 1, embedding_dim))
+        self.transformer_encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=embedding_dim, nhead=4),
+            num_layers=4
+        )
 
+    def forward(self, x):
+        # x is [batch_size, 1, num_specpixels] 
+        x = self.spec_embedding(x)  # Convert each spectrum to an embedding
+        # x += self.positional_encodings  # Add positional encodings
+        x = self.transformer_encoder(x)  # Pass through the transformer encoder
+        return x
 
+class SpectrumTransformerDecoder(nn.Module):
+    def __init__(self, num_specpixels, embedding_dim, output_dim):
+        super(SpectrumTransformerDecoder, self).__init__()
+        # Assuming the same embedding dimension for simplicity and symmetry
+        self.decoder_embedding = nn.Linear(num_specpixels, embedding_dim)
+        # self.positional_encodings = nn.Parameter(torch.zeros(1, 1, embedding_dim))
+        
+        # Decoder Layers: Same configuration as the encoder but using TransformerDecoderLayer
+        self.transformer_decoder = nn.TransformerDecoder(
+            nn.TransformerDecoderLayer(d_model=embedding_dim, nhead=4),
+            num_layers=4
+        )
+
+        # Final layer to transform back to spectrum dimensionality
+        self.output_layer = nn.Linear(embedding_dim, num_specpixels)
+
+    def forward(self, target, encoded_output):
+        # Embed and add positional encodings to the target patches
+        memory = self.decoder_embedding(encoded_output)
+        # memory += self.positional_encodings
+        
+        # Decoding the patches with attention to the memory from the encoder
+        decoded_patches = self.transformer_decoder(target, memory)
+        decoded_patches = self.output_layer(decoded_patches.squeeze(1))
+        
+        # Transform to original patch dimensionality
+        return self.output_layer(decoded_patches)
+
+class SpectrumTransformer(nn.Module):
+    def __init__(self, num_specpixels, embedding_dim, output_dim):
+        super(SpectrumTransformer, self).__init__()
+        self.encoder = SpectrumTransformerEncoder(num_specpixels, embedding_dim)
+        self.decoder = SpectrumTransformerDecoder(num_specpixels, embedding_dim, output_dim)
+
+    def forward(self, x):
+        encoded_output = self.encoder(x)
+        decoded_output = self.decoder(x, encoded_output)
+        return decoded_output
--- tests/test_transformer_ae.py
+++ tests/test_transformer_ae.py
@@ -9,48 +9,49 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from src.data_processing import array_to_tensor, CustomDataset, replace_outliers, apply_mask, rescale_data
-from src.model import AutoEncoderConv
+from src.model import SpectrumTransformer
 from src.train import train_model
 from torchsummary import summary
 import time
 
 # Load the data
-mask = fits.getdata('/Users/mandychen/sky_subtraction/PKS0405-123_OB1/skymask_badpixel_removed.fits')
-cube = Cube('/Users/mandychen/sky_subtraction/PKS0405-123_OB1/DATACUBE_FINAL_EXP1.fits')
-data = cube.data.data
-data = data[10:-10, :, :] # get rid of the first 10 and last 10 wavelength pixels because they are quite noisy
+# mask = fits.getdata('/Users/mandychen/sky_subtraction/PKS0405-123_OB1/skymask_badpixel_removed.fits')
+# cube = Cube('/Users/mandychen/sky_subtraction/PKS0405-123_OB1/DATACUBE_FINAL_EXP1.fits')
+# data = cube.data.data
+# data = data[10:-10, :, :] # get rid of the first 10 and last 10 wavelength pixels because they are quite noisy
 
-# Preprocess the data, clipping outliers and normalizing etc.
-# could also mask nan with data = np.nan_to_num(data) if needed
-sky_spec = apply_mask(data, mask, extract_value=0)
-sky_spec = replace_outliers(sky_spec, lower=3, upper=10, fill_value_lower='median', fill_value_upper='median')
-dc_offset = - np.min(sky_spec) + 10
-sky_spec = sky_spec + dc_offset
-sky_spec, min_val, max_val = rescale_data(sky_spec, log=True, new_min=0, new_max=1, output_minmax=True)
-np.savetxt('/Users/mandychen/sky_subtraction/PKS0405-123_OB1/EXP1_dataprocessing_pars.txt', 
-            [min_val, max_val, dc_offset], delimiter=' ', comments='# min_val, max_val, dc_offset')
+# # Preprocess the data, clipping outliers and normalizing etc.
+# # could also mask nan with data = np.nan_to_num(data) if needed
+# sky_spec = apply_mask(data, mask, extract_value=0)
+# sky_spec = replace_outliers(sky_spec, lower=3, upper=10, fill_value_lower='median', fill_value_upper='median')
+# dc_offset = - np.min(sky_spec) + 10
+# sky_spec = sky_spec + dc_offset
+# sky_spec, min_val, max_val = rescale_data(sky_spec, log=True, new_min=0, new_max=1, output_minmax=True)
+# np.savetxt('/Users/mandychen/sky_subtraction/PKS0405-123_OB1/EXP1_dataprocessing_pars.txt', 
+#             [min_val, max_val, dc_offset], delimiter=' ', comments='# min_val, max_val, dc_offset')
 
-# # Convert the input array to a tensor
+# # # Convert the input array to a tensor
+# sky_spec = sky_spec[:,:10] # only use the first 10 spectra for testing
+# np.savetxt('/Users/mandychen/sky_subtraction/PKS0405-123_OB1/testdata.txt', sky_spec)
+sky_spec = np.loadtxt('/Users/mandychen/sky_subtraction/PKS0405-123_OB1/testdata.txt')
 input_tensor = array_to_tensor(sky_spec)
-# print(input_tensor.size())
+print(input_tensor.size())
 
-# t0 = time.time()
-# # Create an instance of the Autoencoder model
-# enc_in_channels, enc_out_channels = 1, 32
-# enc_kernel_size, enc_stride, enc_padding = 8, 1, 'same'
-# dec_in_channels, dec_out_channels = enc_out_channels, enc_out_channels
-# dec_kernel_size, dec_stride, dec_padding = 6, 2, 0
-# autoencoder = AutoEncoderConv(enc_in_channels, enc_out_channels, enc_kernel_size, enc_stride, enc_padding, 
-#                  dec_in_channels, dec_out_channels, dec_kernel_size, dec_stride, dec_padding)
+t0 = time.time()
+# Create an instance of the Autoencoder model
+num_specpixels, embedding_dim = input_tensor.size(2), 128
+output_dim = num_specpixels
+print(num_specpixels, embedding_dim, output_dim)
+autoencoder = SpectrumTransformer(num_specpixels, embedding_dim, output_dim)
 
-# # summary(autoencoder, (1, length))
+summary(autoencoder, (10, num_specpixels))
 
-# # # Pass the input tensor through the autoencoder
-# output_tensor = autoencoder(input_tensor)
-# print('time takes', time.time() - t0)
-# # Print the output tensor
-# print(output_tensor)
-# print(output_tensor.size())
+# # Pass the input tensor through the autoencoder
+output_tensor = autoencoder(input_tensor)
+print('time takes', time.time() - t0)
+# Print the output tensor
+print(output_tensor)
+print(output_tensor.size())
 
 
 # # test the model with 5 epochs
