df1b2336e9eac2b69505558a2ddd47088d7ad5ab
mandychen-astro <chenmandy2012@gmail.com>
2024-05-20 15:50:36 -0500

    Refactor data processing code for min-max scaling and update file names for PKS0405_HE0226_J1427

--- experiments/PKS0405_HE0226_J1427/train.py
+++ experiments/PKS0405_HE0226_J1427/train.py
@@ -20,10 +20,10 @@ print(torch.cuda.device_count())
 print(torch.cuda.get_device_name())
 
 # Load the data
-train_tensor = torch.load(data_path + 'PKS0405_HE0226_J1427_input_tensor_train_v3.pt')
-val_tensor = torch.load(data_path + 'PKS0405_HE0226_J1427_input_tensor_val_v3.pt')
-train_tensor_clipped = torch.load(data_path + 'PKS0405_HE0226_J1427_input_tensor_train_clipped_v3.pt')
-val_tensor_clipped = torch.load(data_path + 'PKS0405_HE0226_J1427_input_tensor_val_clipped_v3.pt')
+train_tensor = torch.load(data_path + 'PKS0405_HE0226_J1427_input_tensor_train_v4.pt')
+val_tensor = torch.load(data_path + 'PKS0405_HE0226_J1427_input_tensor_val_v4.pt')
+train_tensor_clipped = torch.load(data_path + 'PKS0405_HE0226_J1427_input_tensor_train_clipped_v4.pt')
+val_tensor_clipped = torch.load(data_path + 'PKS0405_HE0226_J1427_input_tensor_val_clipped_v4.pt')
 print(train_tensor.size())
 print(val_tensor.size())
 
@@ -51,8 +51,8 @@ val_loader = DataLoader(val_data, batch_size=64, shuffle=False)
 trained_model, train_loss, val_loss = train_model(model=autoencoder, train_loader=train_loader, 
                 criterion=criterion, optimizer=optimizer, val_loader=val_loader,
                 return_train_loss=True, return_val_loss=True, scheduler=scheduler,
-                num_epochs=20, device='cuda')
+                num_epochs=30, device='cuda')
 
-torch.save(autoencoder.state_dict(), '../../models/model_PKS0405_HE0226_J1427_state_dict_v6.pth')
-np.savetxt('../../models/model_PKS0405_HE0226_J1427_train_loss_v6.txt', train_loss)
-np.savetxt('../../models/model_PKS0405_HE0226_J1427_val_loss_v6.txt', val_loss)
+torch.save(autoencoder.state_dict(), '../../models/model_PKS0405_HE0226_J1427_state_dict_v7.pth')
+np.savetxt('../../models/model_PKS0405_HE0226_J1427_train_loss_v7.txt', train_loss)
+np.savetxt('../../models/model_PKS0405_HE0226_J1427_val_loss_v7.txt', val_loss)
